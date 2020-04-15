d2bb626dee8fcfedf7756e2be46a7156a1123243
Aaron Tadina <atadina@mail.greenriver.edu>
2020-04-15 17:46:37 -0400

    added controller and homepage view

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+require_once('vendor/autoload.php');
+
+$f3 = Base::instance();
+
+$f3->route('GET /', function(){
+//    echo '<h1>Hello World</h1>';
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+
+$f3->run();
